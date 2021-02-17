0e27eb34b719f6268a1727fc98c874dfd535537a
isabelarivera <ir59@njit.edu>
2021-02-16 22:22:14 -0500

    homework

---
+++ HW-FlowControl/src/MyOperators.java
@@ -0,0 +1,16 @@
+
+public class MyOperators {
+	public static void main(String[] args) {
+		System.out.println("Java Exercise [Operators]  Exercise [1]");
+		System.out.println(10 * 5);
+		System.out.println("Java Exercise [Operators]  Exercise [2]");
+		System.out.println(10 / 5);
+		System.out.println("Java Exercise [Operators]  Exercise [3]");
+		int x = 10;
+		++x;
+		System.out.println("Java Exercise [Operators]  Exercise [4]");
+		// int x = 10;
+		x += 5;
+	}
+
+}
