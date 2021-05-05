dc79a1fb40c121294c29938cd20f7f361b7d099b
isabelarivera <ir59@njit.edu>
2021-05-04 20:59:55 -0400

    milestone 3 commits

--- SocketsPart7/src/server/Room.java
+++ SocketsPart7/src/server/Room.java
@@ -1,23 +1,35 @@
 package server;
 
 import java.util.ArrayList;
+
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+//import client.Player;
 
 public class Room implements AutoCloseable {
     private static SocketServer server;// used to refer to accessible server functions
     private String name;
     private final static Logger log = Logger.getLogger(Room.class.getName());
-
+    
+    private List<ServerThread> clients = new ArrayList<ServerThread>();
+    
     // Commands
     private final static String COMMAND_TRIGGER = "/";
     private final static String CREATE_ROOM = "createroom";
     private final static String JOIN_ROOM = "joinroom";
-
+    private final static String FLIP = "flip";
+    private final static String ROLL = "roll";
+    private final static String MUTE = "mute";
+    private final static String UNMUTE = "unmute";
+    
     public Room(String name) {
 	this.name = name;
+
     }
 
     public static void setServer(SocketServer server) {
@@ -28,11 +40,20 @@ public class Room implements AutoCloseable {
 	return name;
     }
 
-    private List<ServerThread> clients = new ArrayList<ServerThread>();
-
     protected synchronized void addClient(ServerThread client) {
 	client.setCurrentRoom(this);
-	if (clients.indexOf(client) > -1) {
+	boolean exists = false;
+	// since we updated to a different List type, we'll need to loop through to find
+	// the client to check against
+	Iterator<ServerThread> iter = clients.iterator();
+	while (iter.hasNext()) {
+	    ServerThread c = iter.next();
+	    if (c == client) {
+		exists = true;
+		}
+		break;
+	}
+	if (exists) {
 	    log.log(Level.INFO, "Attempting to add a client that already exists");
 	}
 	else {
@@ -50,13 +71,20 @@ public class Room implements AutoCloseable {
 	while (iter.hasNext()) {
 	    ServerThread c = iter.next();
 	    if (c != client) {
-		boolean messageSent = client.sendConnectionStatus(c.getClientName(), true, null);
+		client.sendConnectionStatus(c.getClientName(), true, null);
 	    }
 	}
     }
 
     protected synchronized void removeClient(ServerThread client) {
-	clients.remove(client);
+    	Iterator<ServerThread> iter = clients.iterator();
+    	while (iter.hasNext()) {
+    	    ServerThread c = iter.next();
+    	    if (c == client) {
+    		iter.remove();
+    		log.log(Level.INFO, "Removed client " + c.getClientName() + " from " + getName());
+    	    }
+    	}
 	if (clients.size() > 0) {
 	    // sendMessage(client, "left the room");
 	    sendConnectionStatus(client, false, "left the room " + getName());
@@ -88,7 +116,31 @@ public class Room implements AutoCloseable {
     protected void joinLobby(ServerThread client) {
 	server.joinLobby(client);
     }
-
+    
+    protected void createRoom(String room, ServerThread client) {
+	if (server.createNewRoom(room)) {
+	    sendMessage(client, "Created a new room");
+	    joinRoom(room, client);
+	}
+    }
+    
+    //flip and roll methods
+    Random flipRoll = new Random();
+    protected synchronized void roll(ServerThread client) {
+    	int result = flipRoll.nextInt(21);
+    	sendMessage(client, "<b><i><font color=blue> rolled a " + result + "</font></i></b>");
+    }
+     
+    protected synchronized void flip(ServerThread client) {
+    	int result = flipRoll.nextInt(2);
+    	String message;
+    	if(result == 0)
+    		message = " flipped heads";
+    	else 
+    		message = " flipped tails";
+    	sendMessage(client, "<b><i><font color=red>" + message + "</font></i></b>");
+    }
+    
     /***
      * Helper function to process messages to trigger different functionality.
      * 
@@ -96,11 +148,13 @@ public class Room implements AutoCloseable {
      * @param client  The sender of the message (since they'll be the ones
      *                triggering the actions)
      */
+    
     private boolean processCommands(String message, ServerThread client) {
 	boolean wasCommand = false;
 	try {
-	    if (message.indexOf(COMMAND_TRIGGER) > -1) {
-		String[] comm = message.split(COMMAND_TRIGGER);
+		if (message.indexOf(COMMAND_TRIGGER) > -1) {
+		String[] comm = message.split(COMMAND_TRIGGER, 2);
+		
 		log.log(Level.INFO, message);
 		String part1 = comm[1];
 		String[] comm2 = part1.split(" ");
@@ -108,6 +162,7 @@ public class Room implements AutoCloseable {
 		if (command != null) {
 		    command = command.toLowerCase();
 		}
+		
 		String roomName;
 		switch (command) {
 		case CREATE_ROOM:
@@ -122,9 +177,248 @@ public class Room implements AutoCloseable {
 		    joinRoom(roomName, client);
 		    wasCommand = true;
 		    break;
+		case FLIP:
+		    flip(client);
+		    wasCommand = true;
+		    break;
+	    case ROLL:
+		    roll(client);
+		    wasCommand = true;
+		    break;
+	    case MUTE: 
+	    	String[] muted = comm2[1].split(", ");
+	    	List<String> muteList = new ArrayList<String>();
+	    	// can mute multiple clients separated by comma
+	    	for (String user : muted) {
+	    		if (!client.isMuted(user)) {
+		    		client.mute(user);
+		    		muteList.add(user);
+		    	}
+	    	}
+	   
+	    	wasCommand = true;
+	    	break;
+	    case UNMUTE:
+	    	String[] unmuted = comm2[1].split(", ");
+	    	List<String> unmuteList = new ArrayList<String>();
+	    	for (String user : unmuted) {
+	    		if (client.isMuted(user)) {
+	    			client.unmute(user);
+	    			unmuteList.add(user);
+		    	}
+	    	}
+	    	
+	    	wasCommand = true;
+	    	break;
 		}
+		} 
+		// private message functionality
+		// message will be sent to every user specified with an "@" 
+		//ex. "@user1 @user2 This message will be sent to user1 and user2."
+		else if (message.indexOf("@") > -1) {
+			String command = "";
+			String[] comm = message.split("@", 2);
+	
+			String part1 = comm[1];
+			String[] comm2 = part1.split(" @");
+			List<String> users = new ArrayList<String>();
+				// get list of intended users 
+			for (String user : comm2) {
+				if(!user.equals(comm2[comm2.length-1])) {
+					users.add(user.toLowerCase());
+				}
+				else {	// get message
+					String[] pm = user.split(" ", 2);
+					String last = pm[0];
+					users.add(last.toLowerCase());
+					command = pm[1];
+				}
+			}
+			users.add(client.getClientName());
+			sendPrivateMessage(client, "<b> /pm </b> " + command, users);
+	
+			wasCommand = true;
+		}
+		//text font changes
+		//bold font
+		else {
+			String command = message;
+
+			if (command.matches("(.*)\\*(.+)\\*(.*)")) {
+				int count = 0;
+				String changeText = "";
+			ArrayList<String> tags = new ArrayList<String>();
+			for (int i = 0; i < command.length(); i++) {
+				if (Character.toString(command.charAt(i)).equals("*")) {
+					count++;
+					if (count %2 != 0) {
+						tags.add("<b>");
+					}
+					else {
+						tags.add("</b>");
+					}
+					
+				}
+			}
+			String [] bold = command.split("\\*");
+			
+			//"***" potential text option
+			if (bold.length == 0) {
+				changeText += "<b>*</b>";
+						if (command.length() > 3)
+					changeText += command.substring(3);
+				
+			}
+			for (int i = 0; i < bold.length; i++) {
+				
+				// accounts for odd number of "*"
+				//  and also two "*" in a row
+				if (tags.size() == 1 && tags.get(tags.size()-1).contains("<b>") || (bold[i].equals("") && (bold[i+1].equals("")))) {
+					changeText += bold[i] + "*";
+					tags.remove(0);
+				}
+				// convert "**" pairs to "<b></b>"
+				else if (tags.size() > 1 || (tags.size() == 1 && tags.get(tags.size()-1).contains("</b>")) ){
+					changeText += bold[i] + tags.get(0);
+					tags.remove(0);
+				}
+				else changeText += bold[i];
+				}
+	    	wasCommand = true;
+	    	
+	    	// makes conditions stack; 
+	    	if (changeText != "") {
+				command = changeText;
+			}
 	    }
+			//italics font
+			if (command.matches("(.*)_(.+)_(.*)")) {
+				int count = 0;
+				String changeText = "";
+				ArrayList<String> tags = new ArrayList<String>();
+				for (int i = 0; i < command.length(); i++) {
+					if (Character.toString(command.charAt(i)).equals("_")) {
+						count++;
+						if (count %2 != 0) {
+							tags.add("<i>");
+						}
+						else {
+							tags.add("</i>");
+						}
+					}
+				}
+				String [] italics = command.split("_");
+				if (italics.length == 0) {
+					changeText += "<i>_</i>";
+							if (command.length() > 3)
+						changeText += command.substring(3);
+					
+				}
+				for (int i = 0; i < italics.length; i++) {
+					if (tags.size() == 1 && tags.get(tags.size()-1).contains("<i>") || (italics[i].equals("") && (italics[i+1].equals("")))) {
+						changeText += italics[i] + "_";
+						tags.remove(0);
+					}
+					else if (tags.size() > 1 || (tags.size() == 1 && tags.get(tags.size()-1).contains("</i>")) ){ //if (i % 2 == 0) {
+						changeText += italics[i] + tags.get(0);
+						tags.remove(0);
+					}
+					else changeText += italics[i];
+					}
+		    	wasCommand = true;
+		    	if (changeText != "") {
+					command = changeText;
+				}
+	    }
+			// underline font
+			if (command.matches("(.*)~(.+)~(.*)")) {
+				int count = 0;
+				String changeText = "";
+				ArrayList<String> tags = new ArrayList<String>();
+				for (int i = 0; i < command.length(); i++) {
+					if (Character.toString(command.charAt(i)).equals("~")) {
+						count++;
+						if (count %2 != 0) {
+							tags.add("<u>");
+						}
+						else {
+							tags.add("</u>");
+						}
+						
+					}
+				}
+				String [] underline = command.split("~");
+				if (underline.length == 0) {
+					changeText += "<b>~</b>";
+							if (command.length() > 3)
+						changeText += command.substring(3);
+					
+				}			
+				for (int i = 0; i < underline.length; i++) {
+					if (tags.size() == 1 && tags.get(tags.size()-1).contains("<u>") || (underline[i].equals("") && (underline[i+1].equals("")))) {
+						changeText += underline[i] + "~";
+						tags.remove(0);
+					}
+					else if (tags.size() > 1 || (tags.size() == 1 && tags.get(tags.size()-1).contains("</i>")) ){ //if (i % 2 == 0) {
+						changeText += underline[i] + tags.get(0);
+						tags.remove(0);
+					}
+					else changeText += underline[i];
+					}
+		    	wasCommand = true;
+		    	if (changeText != "") {
+					command = changeText;
+				}
+	    }
+	
+		 // color font, add color between two pound signs, use null for black
+			if (command.matches("(.*)#(.+)#(.*)")) {
+				String changeText = "";
+				String colorString = "black";;
+				String [] color = command.split("#", -1);
+				System.out.println(Arrays.toString(color));
+				if (color.length == 0) {
+					changeText += command;	
+				}	
+					for (int i = 0; i < color.length; i++) {
+						
+						if (i % 2 != 0 && (!color[i].contains(" "))){
+							// accounts for odd number of #
+							if (i == color.length-1) {
+								changeText += "#" + color[i];
+							}
+							else { 
+								//if text is between two pound signs declare that as color variable
+								colorString = color[i];
+								if (colorString.equals("")) { colorString = "black"; }
+							}
+						}
+						// will not work if whitespace between pound signs i.e. # #
+						else if (i % 2 != 0 && (color[i].contains(" "))){
+							changeText += "#" + color[i] + "#";
+						}
+						// append message
+						else { 
+							changeText += "<font color="+colorString+">" + color[i] + "</font>";
+							colorString="black";
+						}
+					}
+				wasCommand = true;
+				if (changeText != "") {
+					command = changeText;
+				}
+				
+			}
+	   
+	    	if (wasCommand == true) {
+		    	sendMessage(client, command);
+		}
+
+		}
 	}
+	//catch (EOFException e) {
+		   // ... this is fine
+	//	}
 	catch (Exception e) {
 	    e.printStackTrace();
 	}
@@ -138,8 +432,8 @@ public class Room implements AutoCloseable {
 	    ServerThread c = iter.next();
 	    boolean messageSent = c.sendConnectionStatus(client.getClientName(), isConnect, message);
 	    if (!messageSent) {
-		iter.remove();
-		log.log(Level.INFO, "Removed client " + c.getId());
+			iter.remove();
+			log.log(Level.INFO, "Removed client " + c.getId());
 	    }
 	}
     }
@@ -161,14 +455,41 @@ public class Room implements AutoCloseable {
 	Iterator<ServerThread> iter = clients.iterator();
 	while (iter.hasNext()) {
 	    ServerThread client = iter.next();
-	    boolean messageSent = client.send(sender.getClientName(), message);
-	    if (!messageSent) {
-		iter.remove();
-		log.log(Level.INFO, "Removed client " + client.getId());
+	    	// sender not muted = send message
+	    if (!client.isMuted(sender.getClientName())){
+	    	boolean messageSent = client.send(sender.getClientName(), message);
+		    if (!messageSent) {
+		    	iter.remove();
+		    }
 	    }
 	}
     }
-
+    
+    protected void sendPrivateMessage(ServerThread sender, String message, List<String> users) {
+	log.log(Level.INFO, getName() + ": Sending message to " + users.size() + " clients");
+	if (processCommands(message, sender)) {
+	    // it was a command, don't broadcast
+	    return;
+	}
+	Iterator<ServerThread> iter = clients.iterator();
+	while (iter.hasNext()) {
+	    ServerThread client = iter.next();
+	    	// send message if sender not muted
+	    if(users.contains(client.getClientName().toLowerCase())) {
+	    	if (!client.isMuted(sender.getClientName())){
+	    		boolean messageSent = client.send(sender.getClientName(), message);
+			    if (!messageSent) {
+			    	iter.remove();
+			    }
+	    	}
+	    }
+	}
+    }
+    
+    
+    public List<String> getRooms(String search) {
+    	return server.getRooms(search);
+        }
     /***
      * Will attempt to migrate any remaining clients to the Lobby room. Will then
      * set references to null and should be eligible for garbage collection
@@ -181,9 +502,9 @@ public class Room implements AutoCloseable {
 	    Iterator<ServerThread> iter = clients.iterator();
 	    Room lobby = server.getLobby();
 	    while (iter.hasNext()) {
-		ServerThread client = iter.next();
-		lobby.addClient(client);
-		iter.remove();
+			ServerThread client = iter.next();
+			lobby.addClient(client);
+			iter.remove();
 	    }
 	    log.log(Level.INFO, "Done Migrating " + clients.size() + " to Lobby");
 	}
